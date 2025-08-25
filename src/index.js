f3aa628d6d598639cf6082666ece325067136a9e
AnnaTashSi <si9110219358@gmail.com>
2025-08-25 21:40:26 +0300

    docs: solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-  // Функция parseInt возвращает целое число
+  // Функция parseInt возвращает целое число из строк
   return parseInt(n.toString().split('').reverse().join(''))
 }
 console.log(162);
